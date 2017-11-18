ca9dbc05ee47747edf586f7c923c5fc5983a6477
Rohit Patel <rohit.patel.mails@gmail.com>
2017-11-19 00:18:33 +0530

    remove unused import

--- src/main/java/com/ecc/user/entity/User.java
+++ src/main/java/com/ecc/user/entity/User.java
@@ -4,11 +4,8 @@
 package com.ecc.user.entity;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
